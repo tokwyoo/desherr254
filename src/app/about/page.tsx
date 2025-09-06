53368867c6ea5fa188d84116015bfbb72fe7bb1f
eli <ebmv03@gmail.com>
2025-09-05 17:44:11 -0700

    agrega estructura del proyecto

---
+++ src/app/about/page.tsx
@@ -0,0 +1,51 @@
+export default function Page() {
+    return(
+        <>
+        <div>
+
+            <p className="mb-2">
+                A 14 y/o Mexican person who draws, makes music, occasionally charts, and is still failing at coding. <br/>
+                Native Spanish speaker, fluent in English, and learning Russian (русский). <br/>
+            </p>
+            <p className="mb-2">
+                Before getting into the FNF community I used to record gameplays and upload them to my YT Channel (those videos are currently private), and I also made animations on FlipaClip (though I never uploaded them).
+            </p >
+            <p className="mb-6">
+                Before being &#34;peak&#34;, I used to make really bad music, really bad art and especially the worst animations ever XD. Never stop doing what you love, eventually you&#39;ll be proud of the results ^^
+            </p>
+            <p className="mb-2">
+                Fun facts about me:
+            </p>
+            <p className="mb-2">
+                Favorite Foods:
+                Sushi, Entomatadas, Pizza, Red Rice, Meatballs Soup, Eggs with chorizo, Eggs with bacon, Eggs with ham, Sliced Eggs, Scrambled Eggs
+            </p>
+            <p className="mb-2">
+                Favorite Drinks:
+                CocaCola, Sprite
+            </p>
+            <p className="mb-2">
+                Favorite Weathers:
+                Cloudy days, Rainy days
+            </p>
+            <p className="mb-2">
+                Favorite Games:
+                FNF, Minecraft, Shell Shockers, Roblox, Stardew Valley
+            </p>
+            <p className="mb-2">
+                Idols (music):
+                Fabicholas, KrakenPower, Smily, kiwiquest, Sanko, d4rkwinged, RixFX, Simnsnd and many more people ^^
+            </p>
+            <p className="mb-2">
+                Usernames History:
+                Desumny, JayDeuz_, JayDoodlz_, KinLuveL, RenderDizzter, Cookrizz, NoticlipYT, Desherr
+            </p>
+            <p className="mb-2">
+                Favorite Mods:
+                Ourple Guy, 5Rubles, Mexipiso, Flavor Rave, FunkSpace, Cyber Sensation, From the Top, Vs. Impostor, Vs. Gorefield, Vs. Ziffy
+            </p>
+        </div>
+        </>
+    )
+
+}
---
+++ src/app/friends/page.tsx
@@ -0,0 +1,46 @@
+import {red} from "next/dist/lib/picocolors";
+
+export default function Page() {
+    return(
+        <>
+            {/*================================================================
+                    INICIO "/friends"
+            ================================================================*/}
+
+            <div className="w-full h-full">
+
+                <h1 className="font-bold text-2xl mb-2">Work in progress...</h1>
+
+                <p className="mb-2">Here are some of my friends:</p>
+
+                <ul className="columns-3">
+                    <li>Lonely_Kris</li>
+                    <li>KiroNDM</li>
+                    <li>loocssj</li>
+                    <li>Metalick</li>
+                    <li>Rosmy_Pato</li>
+                    <li>RedBoy</li>
+                    <li>Pikandy</li>
+                    <li>Orxlend</li>
+                    <li>Ori_Neo</li>
+                    <li>KedoNuts</li>
+                    <li>Nyaver</li>
+                    <li>Nerm Mermal</li>
+                    <li>Doccys</li>
+                    <li>Mellopix</li>
+                    <li>Pabj_Z</li>
+                    <li>PixelURU</li>
+                    <li>Doozie</li>
+                    <li>Polly</li>
+                    <li>Mido Diamond</li>
+                </ul>
+
+            </div>
+
+            {/*================================================================
+                    FIN "/friends"
+            ================================================================*/}
+        </>
+    )
+
+}
