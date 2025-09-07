105599c9e946fd9b4986c0d71928d388391f76b8
eli <ebmv03@gmail.com>
2025-09-07 00:33:07 -0700

    agrega pequeños cambios de estilo en /about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,36 +13,32 @@ export default function Page() {
             <p className="mb-6">
                 Before being &#34;peak&#34;, I used to make really bad music, really bad art and especially the worst animations ever XD. Never stop doing what you love, eventually you&#39;ll be proud of the results ^^
             </p>
-            <p className="mb-2">
+
+            <p className="mb-2 font-bold">
                 Fun facts about me:
             </p>
+
             <p className="mb-2">
-                Favorite Foods:
-                Sushi, Entomatadas, Pizza, Red Rice, Meatballs Soup, Eggs with chorizo, Eggs with bacon, Eggs with ham, Sliced Eggs, Scrambled Eggs
+                <span className="underline">Favorite Foods:</span> Sushi, Entomatadas, Pizza, Red Rice, Meatballs Soup, Eggs with chorizo, Eggs with bacon, Eggs with ham, Sliced Eggs, Scrambled Eggs
+            </p>
+
+            <p className="mb-2">
+                <span className="underline">Favorite Drinks:</span> CocaCola, Sprite
             </p>
             <p className="mb-2">
-                Favorite Drinks:
-                CocaCola, Sprite
+                <span className="underline">Favorite Weathers:</span> Cloudy days, Rainy days
             </p>
             <p className="mb-2">
-                Favorite Weathers:
-                Cloudy days, Rainy days
+                <span className="underline">Favorite Games:</span> FNF, Minecraft, Shell Shockers, Roblox, Stardew Valley
             </p>
             <p className="mb-2">
-                Favorite Games:
-                FNF, Minecraft, Shell Shockers, Roblox, Stardew Valley
+                <span className="underline">Idols (music):</span> Fabicholas, KrakenPower, Smily, kiwiquest, Sanko, d4rkwinged, RixFX, Simnsnd and many more people ^^
             </p>
             <p className="mb-2">
-                Idols (music):
-                Fabicholas, KrakenPower, Smily, kiwiquest, Sanko, d4rkwinged, RixFX, Simnsnd and many more people ^^
+                <span className="underline">Usernames History:</span> Desumny, JayDeuz_, JayDoodlz_, KinLuveL, RenderDizzter, Cookrizz, NoticlipYT, Desherr
             </p>
             <p className="mb-2">
-                Usernames History:
-                Desumny, JayDeuz_, JayDoodlz_, KinLuveL, RenderDizzter, Cookrizz, NoticlipYT, Desherr
-            </p>
-            <p className="mb-2">
-                Favorite Mods:
-                Ourple Guy, 5Rubles, Mexipiso, Flavor Rave, FunkSpace, Cyber Sensation, From the Top, Vs. Impostor, Vs. Gorefield, Vs. Ziffy
+                <span className="underline">Favorite Mods:</span> Ourple Guy, 5Rubles, Mexipiso, Flavor Rave, FunkSpace, Cyber Sensation, From the Top, Vs. Impostor, Vs. Gorefield, Vs. Ziffy
             </p>
         </div>
         </>
